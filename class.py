4cc0613be9d502230d1b782a51c562006fe98903
Pierre-Louis Tilak <pierre.tilak@delair-tech.com>
2016-02-05 14:05:42 +0100

    Ajout des fichiers / Première version serveur

---
+++ class.py
@@ -0,0 +1,19 @@
+list_ingredient = ['Coca', 'Orange', 'Eau']
+
+class Glass:
+	""" the current glass object"""
+	def __init__(self,level)
+		self.level = level # Pourcent of fill
+
+class Ingredient:
+	def __init__(self,name, position, category):
+		self.name = name
+		self.position = position
+		self.category = category
+
+class Dose:
+	def __init__(self,ingredient,quantity):
+		self.ingredient = ingredient
+		self.quantity = quantity
+
+class Receipe:
---
+++ server.py
@@ -0,0 +1,29 @@
+import random
+import string
+import os,os.path
+import cherrypy
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        return open(os.path.join('./public/index.html'))
+    @cherrypy.expose
+    def generate(self, length=8, cocktail='whiskycoca'):
+        return 'Cocktail ',cocktail,' is Coming'
+#''.join(random.sample(string.hexdigits, int(length)))
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    cherrypy.config.update({'server.socket_port': 8090,
+                        'server.socket_host':'0.0.0.0',
+                        'engine.autoreload_on': False})
+    cherrypy.quickstart(StringGenerator(), '/', conf)
