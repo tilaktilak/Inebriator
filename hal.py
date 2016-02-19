5c87998ed9285a2d36ecda4f61132f44cd16f1e8
tilaktilak <pierre.tilak@gmail.com>
2016-02-19 08:49:18 +0100

    Ajout de fonctions dans class + modiffs serveur

--- hal.py
+++ hal.py
@@ -1,25 +1,39 @@
 import RPi.GPIO as GPIO
 import time
-def set_motor(sens, step):
+def set_motor(motor, sens, step):
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(12, GPIO.OUT) # STEP
-	GPIO.setup(11, GPIO.OUT) # DIR
-	if sens == 1:
-		GPIO.output(11,GPIO.HIGH)
+	if motor == 1:
+		STEP = 12
+		DIR = 11
 	else:
-		GPIO.output(11,GPIO.LOW)
+		return
+	GPIO.setup(STEP, GPIO.OUT) # STEP
+	GPIO.setup(DIR, GPIO.OUT) # DIR
+	if sens == 1:
+		GPIO.output(DIR,GPIO.HIGH)
+	else:
+		GPIO.output(DIR,GPIO.LOW)
 	for i in range(0,step):
-		GPIO.output(12,GPIO.LOW)
+		GPIO.output(STEP,GPIO.LOW)
 		time.sleep(0.01)
-		GPIO.output(12,GPIO.HIGH)
+		GPIO.output(STEP,GPIO.HIGH)
 		print(step)
-	GPIO.output(12,GPIO.LOW)
+	GPIO.output(STEP,GPIO.LOW)
+
+def set_angle(sens, step):
+	set_motor(1, sens, step)
+
+def set_up_down(direction):
+	if direction=="up":
+		set_motor(2,1,20)
+	else :
+		set_motor(2,0,20)
 
 def sequence():
-	set_motor(1,10)
+	set_angle(1,10)
 	time.sleep(1)
-	set_motor(0,10)
+	set_angle(0,10)
 	time.sleep(1)
-	set_motor(1,200)
+	set_angle(1,200)
 	time.sleep(1)
-	set_motor(0,200)
+	set_angle(0,200)
