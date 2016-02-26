680f786397de7613e4717988659e4013b923b4fa
tilaktilak <pierre.tilak@gmail.com>
2016-02-26 07:58:19 +0100

    Ajout de méthodes de classe

--- class.py
+++ class.py
@@ -8,6 +8,29 @@ class Glass:
 		self.level = level # Pourcent of fill
 		self.position = position
 
+	def fetch_dose(self,Dose):
+            """ Check wich sens is faster (in step)"""
+                if (self.position - Dose.ingredient.position)>100:
+			sens = 1
+		else :
+			sens = 0
+		set_angle(sens,self.position - Dose.ingredient.position)
+		self.position = Dose.ingredient.position;
+
+		if Dose.ingredient.category == 'Soft':
+			## Enable Soft Pump
+			time.sleep(Dose.quantity)
+			## Disable Soft Pump
+		else:
+			""" Grow up glass	"""
+			time.sleep(Dose.quantity)
+			""" Grow down glass """
+			self.level += Dose.quantity
+
+	def make_cocktail(self,receipe):
+            for dose in receipe:
+                self.fetch_dose(self,Dose)
+
 class Ingredient:
 	def __init__(self,name, position, category):
 		self.name = name
@@ -29,24 +52,4 @@ D_Coca = Dose(Coca,7)
 R_Whiskey_Coca = [D_Whiskey,D_Coca]
 
 MyCup = Glass(0,0)
-def fetch_position(Dose):
-	""" Check wich sens is faster (in step)"""
-	if (MyCup.position - Dose.ingredient.position)>100:
-		sens = 1
-	else :
-		sens = 0
-	set_angle(sens,MyCup.position - Dose.ingredient.position)
-	MyCup.position = Dose.ingredient.position;
-
-	if Dose.ingredient.category == 'Soft':
-		## Enable Soft Pump
-		time.sleep(Dose.quantity)
-		## Disable Soft Pump
-	else:
-		""" Grow up glass	"""
-		time.sleep(Dose.quantity)
-		""" Grow down glass """
-		MyCup.level += Dose.quantity
-
-def make_cocktail(receipe):
 
