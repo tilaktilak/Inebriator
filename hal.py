2ad2bf894e07366e570162cea11a53cdeee0d441
tilaktilak <pierre.tilak@gmail.com>
2016-03-08 08:41:28 +0100

    Ajout de la fonction de montée descendre réglée

--- hal.py
+++ hal.py
@@ -1,49 +1,12 @@
 import RPi.GPIO as GPIO
 import time
-
-def init_motor(motor):
-	GPIO.setmode(GPIO.BOARD)
-	if motor ==1:
-		STEP = 12
-		DIR  = 11
-		FDC  = 13
-        else:
-		return
-	GPIO.setup(STEP,GPIO.OUT)
-	GPIO.setup(DIR,GPIO.OUT)
-	GPIO.setup(FDC, GPIO.IN, GPIO.PUD_UP)
-	""" Define DIR statically """
-	GPIO.output(DIR,GPIO.HIGH)
-	
-	while GPIO.input(FDC) != GPIO.LOW:
-		GPIO.output(STEP,GPIO.HIGH)
-		time.sleep(0.01)
-		GPIO.output(STEP,GPIO.LOW)
-	""" Here we have an idea of zero position """
-	""" Go backward then forward slownly """
-	GPIO.output(DIR,GPIO.LOW)
-	for i in range(0,20):
-		GPIO.output(STEP,GPIO.LOW)
-		time.sleep(0.01)
-		GPIO.output(STEP,GPIO.HIGH)
-		time.sleep(0.01)
-	
-	while GPIO.input(FDC) != GPIO.LOW:
-		GPIO.output(STEP,GPIO.HIGH)
-		time.sleep(0.1)
-		GPIO.output(STEP,GPIO.LOW)
-		time.sleep(0.1)
-	
-# Motor Number, Sens, Nb of step, Speed in rps	
-def set_motor(motor, sens, step, speed):
+def set_motor(motor, sens, step, delay):
 	GPIO.setmode(GPIO.BOARD)
 	if motor == 1:
 		STEP = 12
 		DIR = 11
-                NB_STEP = 200 
 	else:
 		return
-
 	GPIO.setup(STEP, GPIO.OUT) # STEP
 	GPIO.setup(DIR, GPIO.OUT) # DIR
 	if sens == 1:
@@ -51,25 +14,32 @@ def set_motor(motor, sens, step, speed):
 	else:
 		GPIO.output(DIR,GPIO.LOW)
 	for i in range(0,step):
-		GPIO.output(STEP,GPIO.HJGH)
-		time.sleep(speed/(NB_STEP*2))
 		GPIO.output(STEP,GPIO.LOW)
-		time.sleep(speed/(NB_STEP*2))
+		time.sleep(delay)
+		GPIO.output(STEP,GPIO.HIGH)
+		print(step)
+	GPIO.output(STEP,GPIO.LOW)
 
 def set_angle(sens, step):
-	set_motor(1, sens, step,2)
+	motor = 1
+	set_motor(1, sens, step,0.01)
 
 def set_up_down(direction):
+	hauteur = 4700
+	motor = 1
 	if direction=="up":
-		set_motor(2,1,20,2)
+		set_motor(motor,0,hauteur,0.001)
 	else :
-		set_motor(2,0,20,2)
+		set_motor(motor,1,hauteur,0.001)
 
 def sequence():
-	set_angle(1,10)
+	set_up_down("up")
 	time.sleep(1)
-	set_angle(0,10)
-	time.sleep(1)
-	set_angle(1,200)
-	time.sleep(1)
-	set_angle(0,200)
+	set_up_down("down")
+#	set_angle(0,10)
+#	time.sleep(1)
+#	set_angle(1,200)
+#	time.sleep(1)
+#	set_angle(0,200)
+
+#sequence()
